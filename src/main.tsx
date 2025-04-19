e3102d2687dc02c546c8101a85be289e7224a4ca
nil2004 <your-email@example.com>
2025-04-19 22:04:57 +0530

    Add responsive styles for mobile devices

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './styles/responsive.css'  // Import our responsive styles
 import { initializeAdminUser } from './lib/admin-setup'
 
 // Initialize admin user
